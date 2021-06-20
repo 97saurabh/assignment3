9a787fb7a4a1dd47b0175c83c2f681db0df54e1c
Saurabh <singh97saurabh19@gmail.com>
2021-06-21 03:39:35 +0530

    Update

--- main.dart
+++ main.dart
@@ -1,46 +1,32 @@
-import 'Family.dart';
+import 'Node.dart';
 import 'FamilyWrapper.dart';
 main(List<String> args) {
-  var obj1 = Singleton();
-
-  print(obj1.addNode(1, "Saurabh Singh"));
-  print(obj1.addNode(2, "Saurabh Singh"));
-  print(obj1.addNode(3, "Saurabh Singh"));
-  print(obj1.addNode(4, "Saurabh Singh"));
-  print(obj1.addNode(5, "Saurabh Singh"));
-  print(obj1.addNode(6, "Saurabh Singh"));
-  print(obj1.addNode(7, "Saurabh Singh"));
-  print(obj1.addNode(8, "Saurabh Singh"));
-
-  print(obj1.addDependency(1, 2));
-  print(obj1.addDependency(1, 3));
-  print(obj1.addDependency(1, 4));
-  print(obj1.addDependency(1, 5));
-  print(obj1.addDependency(5, 8));
-  print(obj1.addDependency(8, 1));
-
-
-
-
   
-  dynamic data = obj1.data;
+  var family = FamilyUtility();
 
-  dynamic o1 = data[1];
-  print(o1.parents);
-  print(o1.children);
-  print("################################################");
-  dynamic arr = obj1.getDescendantById(1);
-  arr = arr['children'];
-  for (dynamic key in arr){
-    print(key.id);
-  }
-  print(" ");
-  dynamic arr2 = obj1.getAncestorsById(8);
+  print(family.addNode(1, "Saurabh Singh"));// new instance created
+  print(family.addNode(2, "Saurabh Singh"));// new instance created
+  print(family.addNode(3, "Saurabh Singh"));// new instance created
+  print(family.addNode(4, "Saurabh Singh"));// new instance created
+  print(family.addNode(5, "Saurabh Singh"));// new instance created
+  print(family.addNode(6, "Saurabh Singh"));// new instance created
+  print(family.addNode(7, "Saurabh Singh"));// new instance created
+  print(family.addNode(8, "Saurabh Singh"));// new instance created
+
+  family.addDependency(1, 2);// New depency added
+  family.addDependency(1, 3);// New depency added
+  family.addDependency(1, 4);// New depency added
+  family.addDependency(1, 5);// New depency added
   
-  arr = arr2['ancestors'];
-  for (dynamic key in arr){
-    print(key.id);
-  }
+  // It will print Dependency alredy exist
+  family.addDependency(5, 8);
+
+  print(family.getDescendantById(1));// it will return all other nodes
+  print(family.getChildren(1)); // it will return all child of 1
+
+
+
+
 
 
 }
